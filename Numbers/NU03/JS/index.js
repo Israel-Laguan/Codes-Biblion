92a05acc62ebb6d115d754dc4a8742313a422838
Israel-Laguan <israellaguan@gmail.com>
2020-06-27 09:51:35 -0500

    feat: Solve Fibonacci Problem  On branch master  Changes to be committed: 	modified:   Numbers/NU03/JS/index.js 	modified:   Numbers/NU07/JS/index.js

--- Numbers/NU03/JS/index.js
+++ Numbers/NU03/JS/index.js
@@ -1,3 +1,12 @@
-const func = () => {}
+const fibonacciToNth = (n) => {
+  if (!n || n < 0) return 0;
+  if (n < 2) {
+    return [0, 1];
+  } else {
+    let s = fibonacciToNth(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
 
-console.assert(func()==null)
+console.log(fibonacciToNth(8));
--- Numbers/NU07/JS/index.js
+++ Numbers/NU07/JS/index.js
@@ -1,3 +1,16 @@
-const func = () => {}
+/**
+ * Calculate the monthly payments of a fixed term mortgage
+ * over given Nth terms at a given interest rate
+ * @param {Number} amount How much do you have to pay
+ * @param {Number} rate Percertage to pay, 10% = 0.1
+ * @param {Number} time Months
+ * @returns {Number} Mortage to pay monthly
+ */
+const mortageCalculator = (amount = 0, rate = 0.1, time = 12) => {
+  if (amount < 0 || time <= 0 || rate < 0) throw Error("Invalida data!");
+  if (amount === 0) return 0;
 
-console.assert(func()==null)
+  return;
+};
+
+console.assert(mortageCalculator(0) == 0);
